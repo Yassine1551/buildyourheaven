5a5d01f9f600eb1840e360a42c511285ea0be5ea
Yassine1551 <yassine1551@github.com>
2026-08-06 19:12:22 +0100

    حذف سكربتات مؤقتة للفحص (list-titles, peek-temp)

--- scripts/list-titles.mjs
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-const c = fs.readFileSync('C:\\Users\\NISSAY\\Downloads\\buildyourheaven code\\constants\\verses.ts', 'utf8');
-const titles = c.match(/title: '([^']+)'/g).map(t => t.split("'")[1]);
-const orders = c.match(/order: (\d+)/g).map(t => t.split(' ')[1]);
-for (let i = 0; i < titles.length; i++) {
-  console.log(orders[i] + ': ' + titles[i]);
-}
